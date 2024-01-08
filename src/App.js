ff82df5eb4b01a1bef3c067df362c5168e3d3b74
feitan96 <113403122+feitan96@users.noreply.github.com>
2024-01-08 18:43:17 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import { jwtDecode } from 'jwt-decode';
+
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({});
+
+  function handleCallbackResponse(response) {
+    console.log('Encoded JWT ID token: ' + response.credential);
+    var userObject = jwtDecode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById('signInDiv').hidden = true;
+  }
+
+  function handleSignOut(event) {
+    setUser({});
+    const signInDiv = document.getElementById('signInDiv');
+    if (signInDiv) {
+      signInDiv.hidden = false;
+    }
+  }  
+  
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: '468498587009-1anqe651n5ar8tp54mvihofk0kkq5fu9.apps.googleusercontent.com',
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+      theme: 'outline',
+      size: 'large',
+    });
+
+    google.accounts.id.prompt();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {Object.keys(user).length !== 0 ? (
+          <div className="user-profile">
+            <div className="user-info">
+              <img src={user.picture} alt="user-profile-pic" />
+              <h3 className="user-name">{user.name}</h3>
+            </div>
+            <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
+          </div>
+        ) : (
+          <>
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>We calculate your grades for you!</p>
+            <div id="signInDiv"></div>
+          </>
+        )}
       </header>
     </div>
   );
